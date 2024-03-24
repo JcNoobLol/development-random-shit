2278b4edcc51c71ee3209dfcac52be572b078f5c
James Clerick <92348773+JcNoobLol@users.noreply.github.com>
2024-03-24 05:21:36 -0400

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-let express  = require("express");let app = express(); app.listen(3000); console.log("up"); let path = require("path")
-
-app.get('/',(req,res) => {
-    res.sendfile(path.join(__dirname,'src/val.html'))
-})
-app.get("/14",(req,res) => {
-    res.sendfile(path.join(__dirname,'src/14.html'))
-})
